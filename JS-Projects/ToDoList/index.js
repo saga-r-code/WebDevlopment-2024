8cf67978aacd4f35142795891624b9250889dc55
saga-r-code <shah.sagar2006@gmail.com>
2024-07-27 05:18:03 +0530

    todolistcomplete

--- JS-Projects/ToDoList/index.js
+++ JS-Projects/ToDoList/index.js
@@ -1,15 +1,43 @@
-const inputTask = document.body.getElementsByClassName("inputTask")[0]
-const taskList = document.body.getElementsByClassName("task-list")[0]
+const inputTask = document.body.getElementsByClassName("inputTask")[0];
+const taskList = document.body.getElementsByClassName("task-list")[0];
 
-const AddTask = () =>{
-    if(inputTask.value === ''){
-        alert(" Please Write Something")
-    }
-    else{
-        let liTag = document.createElement("li")
-        liTag.innerHTML = inputTask.value
-        taskList.appendChild(liTag)
-    }
+const AddTask = () => {
+  if (inputTask.value === "") {
+    alert(" Please Write Something");
+  } else {
+    let liTag = document.createElement("li");
+    liTag.innerHTML = inputTask.value;
+    taskList.appendChild(liTag);
 
-    inputTask.value = ''
+    let span = document.createElement("span");
+    span.innerHTML = "&#10006";
+    liTag.appendChild(span);
+  }
+
+  inputTask.value = "";
+  SaveData();
+};
+
+taskList.addEventListener(
+  "click",
+  (e) => {
+    // console.log(e);
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      SaveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      SaveData();
+    }
+  },
+  false
+);
+
+function SaveData(){
+  localStorage.setItem("todolist-task", taskList.innerHTML);
+};
+
+function DispalyTask(){
+    taskList.innerHTML = localStorage.getItem("todolist-task")
 }
+DispalyTask()
